88e2eaf498442c7218d7c7b444e13e0ffe9f1822
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-10 06:56:51 +0300

    update

--- src/Components/HomeDetails/Photogrid.jsx
+++ src/Components/HomeDetails/Photogrid.jsx
@@ -5,39 +5,39 @@ import { GrApps } from "react-icons/gr";
 const Photogrid = () => {
   return (
     <div className=" h-90 relative">
-      <div className="w-full flex h-90 md:flex-col ">
-        <div className=" h-90 w-1/2 md:w-full md:m-2">
+      <div className=" flex ">
+        <div className=" h-90 w-full lg:w-[65%]">
           <img
             src="/images/home5.jpg"
             alt=""
-            className="h-full w-full  rounded-l-2xl object-fill hover:brightness-75 cursor-pointer"
+            className="h-full   rounded-l-2xl object-fill hover:brightness-75 cursor-pointer"
           />
         </div>
-        <div className="grid grid-cols-2 md:w-full gap-4  overflow-hidden px-4 hover:brightness-75 cursor-pointer ">
+        <div className="hidden md:grid grid-cols-2 gap-2  overflow-hidden px-2 hover:brightness-75 cursor-pointer ">
           <img
             src="/images/home6.jpg"
             alt=""
-            className="h-48 w-96 rounded-tr-2xl hover:brightness-75 cursor-pointer "
+            className="h-48 w-96  hover:brightness-75 cursor-pointer "
           />
           <img
             src="/images/home7.jpg"
             alt=""
-            className="h-48 w-96 rounded-tr-2xl hover:brightness-75 cursor-pointer"
+            className="h-48 w-96  hover:brightness-75 cursor-pointer"
           />
           <img
             src="/images/home8.jpg"
             alt=""
-            className="h-48 w-96 rounded-tr-2xl hover:brightness-75 cursor-pointer "
+            className="h-48 w-96  hover:brightness-75 cursor-pointer "
           />
           <img
             src="/images/home9.jpg"
             alt=""
-            className="h-48 w-96 rounded-tr-2xl hover:brightness-75 cursor-pointer"
+            className="h-48 w-96  hover:brightness-75 cursor-pointer"
           />
         </div>
       </div>
-      <div className="absolute z-10 gap-2 border bottom-6 right-16 px-4 py-2 bg-white text-black font-semibold rounded-md cursor-pointer items-center flex">
-        <span className="mb-3">
+      <div className="hidden md:absolute z-10  border bottom-6 right-16 px-3 pt-1 bg-white text-black font-semibold rounded-md cursor-pointer flex">
+        <span className="m-1">
           <GrApps />
         </span>
         <p>View all Pictures</p>
