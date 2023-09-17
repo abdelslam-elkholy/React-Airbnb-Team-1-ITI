b3427ae96642c74f80d87d34990ed11480423b1e
Mariam Hesham <mariamhesham414@gmail.com>
2023-09-17 22:43:23 +0200

    rest of account nested pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Account from "./Components/Account/Account";
+import { BrowserRouter } from "react-router-dom";
+import PersonalInfo from "./Components/Account/PersonalInfo/PersonalInfo";
+import LoginAndSecurity from "./Components/Account/LoginAndSecurity/LoginAndSecurity";
+import PaymentsAndPayouts from "./Components/Account/PaymentsAndPayouts/PaymentsAndPayouts";
+import Taxes from "./Components/Account/Taxes/Taxes";
+import Notifications from "./Components/Account/Notifications/Notifications";
+import PrivacyAndSharing from "./Components/Account/PrivacyAndSharing/PrivacyAndSharing";
+import GlobalPreferences from "./Components/Account/GlobalPreferences/GlobalPreferences";
+import TravelForWork from "./Components/Account/TravelForWork/TravelForWork";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      {/* <BrowserRouter> */}
+      {/* <Account /> */}
+      {/* <PersonalInfo /> */}
+      {/* <LoginAndSecurity /> */}
+      {/* <PaymentsAndPayouts /> */}
+      {/* <Taxes /> */}
+      {/* <Notifications /> */}
+      {/* <PrivacyAndSharing /> */}
+      {/* <GlobalPreferences /> */}
+      <TravelForWork />
+      {/* </BrowserRouter> */}
+    </>
+  );
 };
 
 export default App;
--- src/router.jsx
+++ src/router.jsx
@@ -8,6 +8,14 @@ import Trips from "./Components/Trips";
 import NotFound from "./Components/NotFound";
 import Account from "./Components/Account";
 import Reservation from "./Components/Reservation";
+import PersonalInfo from "./Components/Account/PersonalInfo";
+import LoginAndSecurity from "./Components/Account/LoginAndSecurity";
+import PaymentsAndPayouts from "./Components/Account/PaymentsAndPayouts/PaymentsAndPayouts";
+import Taxes from "./Components/Account/Taxes/Taxes";
+import Notifications from "./Components/Account/Notifications/Notifications";
+import PrivacyAndSharing from "./Components/Account/PrivacyAndSharing/PrivacyAndSharing";
+import GlobalPreferences from "./Components/Account/GlobalPreferences/GlobalPreferences";
+import TravelForWork from "./Components/Account/TravelForWork/TravelForWork";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +28,19 @@ const router = createBrowserRouter([
       { path: "/wishlist", element: <WishList /> },
       { path: "/messages", element: <Messages /> },
       { path: "/trips", element: <Trips /> },
-      { path: "/account", element: <Account /> },
+      {
+        path: "/account",
+        element: <Account />,
+        // children: [{ path: "personalInfo", element: <PersonalInfo /> }],
+      },
+      { path: "/personalInfo", element: <PersonalInfo /> },
+      { path: "/loginAndSecurity", element: <LoginAndSecurity /> },
+      { path: "/paymentsAndPayouts", element: <PaymentsAndPayouts /> },
+      { path: "/taxes", element: <Taxes /> },
+      { path: "/notifications", element: <Notifications /> },
+      { path: "/privacyAndSharing", element: <PrivacyAndSharing /> },
+      { path: "/globalPreferences", element: <GlobalPreferences /> },
+      { path: "/travelForWork", element: <TravelForWork /> },
       { path: "/reservation", element: <Reservation /> },
     ],
   },
