1165106a88f30ecdf773531cc97a5d0dd41ff079
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-20 13:43:33 +0300

    uppdate

--- src/router.jsx
+++ src/router.jsx
@@ -5,10 +5,18 @@ import WishList from "./Pages/WishList";
 import Messages from "./Pages/Messages";
 import Trips from "./Pages/Trips";
 import NotFound from "./Pages/NotFound";
-import Account from "./Pages/Account";
 import Reservation from "./Pages/Reservation";
 import Homedetails from "./Pages/Homedetails";
 import Home from "./Pages/Home";
+import Account from "./Pages/Account/Account";
+import PersonalInfo from "./Components/Account/PersonalInfo/PersonalInfo";
+import LoginAndSecurity from "./Components/Account/LoginAndSecurity/LoginAndSecurity";
+import PaymentsAndPayouts from "./Components/Account/PaymentsAndPayouts/PaymentsAndPayouts";
+import Taxes from "./Components/Account/Taxes/Taxes";
+import Notifications from "./Components/Account/Notifications/Notifications";
+import PrivacyAndSharing from "./Components/Account/PrivacyAndSharing/PrivacyAndSharing";
+import GlobalPreferences from "./Components/Account/GlobalPreferences/GlobalPreferences";
+import TravelForWork from "./Components/Account/TravelForWork/TravelForWork";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +31,15 @@ const router = createBrowserRouter([
       { path: "/trips", element: <Trips /> },
       { path: "/account", element: <Account /> },
       { path: "/reservation", element: <Reservation /> },
+      { path: "/account", element: <Account /> },
+      { path: "/personalInfo", element: <PersonalInfo /> },
+      { path: "/loginAndSecurity", element: <LoginAndSecurity /> },
+      { path: "/paymentsAndPayouts", element: <PaymentsAndPayouts /> },
+      { path: "/taxes", element: <Taxes /> },
+      { path: "/notifications", element: <Notifications /> },
+      { path: "/privacyAndSharing", element: <PrivacyAndSharing /> },
+      { path: "/globalPreferences", element: <GlobalPreferences /> },
+      { path: "/travelForWork", element: <TravelForWork /> },
     ],
   },
 
