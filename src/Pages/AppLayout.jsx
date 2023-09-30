7960ddc0844f54c4ab9b92021b9525098fbdf76a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-30 19:59:59 +0300

    update

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import Header from "../Components/header/Header";
 const AppLayout = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
