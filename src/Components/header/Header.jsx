4435d34ce41a24ae1222260fa795520df1d6f6f1
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-02 07:34:39 +0300

    update

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
+import React from "react";
 // components
-import Logo from './Logo';
-import LocationSearch from './LocationSearch';
+import Logo from "./Logo";
+import LocationSearch from "./LocationSearch";
 import {
   flexBetweenCenter,
   dFlex,
   displayOnDesktop,
-} from '../../Themes/commonStyles';
-import ProfileSettings from './ProfileSettings';
-import MobileSearch from './MobileSearch';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-
+} from "../../Themes/commonStyles";
+import ProfileSettings from "./ProfileSettings";
+import MobileSearch from "./MobileSearch";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigator = useNavigate();
+
   return (
     <>
-    <Box 
-      sx={{
-        ...dFlex,
-        minHeight:70,
-        borderBottom:'1px solid #ddd',
-      }}>
+      <Box
+        sx={{
+          ...dFlex,
+          minHeight: 70,
+          borderBottom: "1px solid #ddd",
+        }}
+      >
         <Container maxWidth="xl">
-          <Box sx={{...flexBetweenCenter, 
-            minHeight:90,
-            p:1
-            }}>
-              <Box sx={displayOnDesktop}>
-              <Logo sx={{pr:2}}/>
-              </Box>
-              <Box  sx={displayOnDesktop}>
+          <Box sx={{ ...flexBetweenCenter, minHeight: 90, p: 1 }}>
+            <Box sx={displayOnDesktop} onClick={() => navigator("/home")}>
+              <Logo sx={{ pr: 2 }} />
+            </Box>
+            <Box sx={displayOnDesktop}>
               <LocationSearch />
-              </Box>
-              <Box sx={displayOnDesktop}>
+            </Box>
+            <Box sx={displayOnDesktop}>
               <ProfileSettings />
-              </Box>
-              <Box sx={{ display: { xs: 'flex', md: 'none' } }}> 
-            <MobileSearch/>
-            </Box> 
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <MobileSearch />
+            </Box>
           </Box>
         </Container>
       </Box>
