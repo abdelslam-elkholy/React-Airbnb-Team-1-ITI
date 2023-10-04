432edfdac8a3c012e001735eef77c8451c5b2f76
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-04 18:24:12 +0300

    update

--- src/Pages/Trips/Trips.jsx
+++ src/Pages/Trips/Trips.jsx
@@ -7,12 +7,21 @@ import instance from "../../AxiosConfig/instance";
 
 const Trips = () => {
   const [reservations, setReservations] = useState([]);
+
   useEffect(() => {
     instance.get("/reservations/me").then((res) => {
       setReservations(res.data.data.reservations);
       console.log(res.data.data.reservations);
     });
   }, []);
+
+  const options = { year: "numeric", month: "long", day: "numeric" };
+
+  const formattedDate = (inputDateString) => {
+    const inputDate = new Date(inputDateString);
+
+    return inputDate.toLocaleDateString("en-US", options);
+  };
 
   return (
     <>
@@ -24,7 +33,7 @@ const Trips = () => {
                 <div className="ticket">
                   <div className="ticketTitle px-5 pt-4 d-flex justify-content-between">
                     <div className="text-center col-lg-2 col-md-2 col-3 d-flex flex-column">
-                      <h2 className="mb-1">DUB</h2>
+                      <h2 className="mb-1">{reservation.houseId.name}</h2>
                     </div>
                     <div className="d-flex align-items-center col-lg-8 col-md-8 col-6">
                       <div className="progressTrip ">
@@ -40,22 +49,30 @@ const Trips = () => {
                       </div>
                     </div>
                     <div className="text-center col-lg-2 col-md-2 col-3 d-flex flex-column">
-                      <h2 className="mb-1">YVR</h2>
+                      <h2 className="mb-1">
+                        {(new Date(reservation.checkOut) -
+                          new Date(reservation.checkIn)) /
+                          1000 /
+                          60 /
+                          60 /
+                          24}
+                        /night
+                      </h2>
                     </div>
                   </div>
                   <div className="ticketTitle px-5 pt-4 d-flex justify-content-between text-center">
                     <div className="col-lg-2 col-md-2 col-3">
-                      <h6 className="text-secondary">Depart</h6>
-                      <h6>10:00AM</h6>
+                      <h6 className="text-secondary">Arrive</h6>
+                      <h6>{formattedDate(reservation.checkIn)}</h6>
                     </div>
-                    <div className="col-lg-1 col-md-2 col-3">
+                    {/* <div className="col-lg-1 col-md-2 col-3">
                       <div className="circleProgressTripGreen"></div>
                       <h6>Direct</h6>
                       <h6 className="text-secondary">8hrs 30m</h6>
-                    </div>
+                    </div> */}
                     <div className="col-lg-2 col-md-2 col-3">
-                      <h6 className="text-secondary">Arrive</h6>
-                      <h6>18:30AM</h6>
+                      <h6 className="text-secondary">Depart</h6>
+                      <h6>{formattedDate(reservation.checkOut)}</h6>
                     </div>
                   </div>
                   <div className="ticketRip">
@@ -71,7 +88,7 @@ const Trips = () => {
                       </button>
                     </div>
                     <div className="col-lg-2 col-md-2 col-2">
-                      <h4>$500</h4>
+                      <h4>{reservation.price}</h4>
                     </div>
                   </div>
                 </div>
